065a67598a5c61d8b1a144ff411e27b181e5bde3
Razi D <racieldiz@gmail.com>
2022-05-13 15:00:03 -0700

    changes changes changes

---
+++ .history/ultraradiant/src/components/TheWelcome_20220513145938.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+import WelcomeItem from "./WelcomeItem.vue";
+import DocumentationIcon from "./icons/IconDocumentation.vue";
+import ToolingIcon from "./icons/IconTooling.vue";
+import EcosystemIcon from "./icons/IconEcosystem.vue";
+import CommunityIcon from "./icons/IconCommunity.vue";
+import SupportIcon from "./icons/IconSupport.vue";
+</script>
+
+<<template>
+  <div>
+    <Index/>
+  </div>
+</template>
+
+<script>
+import Index from '@/components/Index.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Index
+  }
+}
+</script>
